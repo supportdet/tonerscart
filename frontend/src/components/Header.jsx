b6f21b51343ba42adb2bbe2b773c7c318c1c621e
supportdet <support@digitaledgeindia.com>
2026-05-24 02:08:36 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -45,7 +45,7 @@ export default function Header() {
                     <img
                         src="/TONERSCART-bg.png"
                         alt="TonersCart"
-                        className="block h-9 w-auto transition-transform group-hover:scale-[1.03]"
+                        className="block h-17 w-auto transition-transform group-hover:scale-[1.03]"
                         style={{ alignSelf: "center" }}
                         data-testid="header-logo-img"
                     />
